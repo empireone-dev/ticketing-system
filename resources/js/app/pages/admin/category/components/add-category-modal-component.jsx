4d900d40ad8cfa54a5b821561fd8c7b51ed91f3b
unknown <Marlou Dev>
2024-06-24 23:53:04 +0800

    added new progress

--- resources/js/app/pages/admin/category/components/add-category-modal-component.jsx
+++ resources/js/app/pages/admin/category/components/add-category-modal-component.jsx
@@ -33,6 +33,7 @@ export default function AddCategoryModalComponent({ isOpen, closeModal }) {
                                     ...data,
                                     name:e.target.value
                                 })}
+                                value={data?.name??''}
                                 type="text"
                                 className="block py-2.5 px-0 w-full text-md text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer"
                                 placeholder=" "
