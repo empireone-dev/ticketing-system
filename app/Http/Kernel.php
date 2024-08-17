b413f3af279db888f56859d26bfedc41c4da0f9c
unknown <Marlou Dev>
2024-08-17 08:56:45 -0400

    kernel adjusmtent

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,9 +41,13 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            'throttle:api',
+            'throttle:60000000000,1',
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\EncryptCookies::class,//this is for Request failed with status code 401
+            \Illuminate\Session\Middleware\StartSession::class,//this is for Request failed with status code 401
         ],
     ];
 
--- routes/api.php
+++ routes/api.php
@@ -25,22 +25,21 @@ Route::middleware('auth:sanctum')->get('/users', function (Request $request) {
     return $request->user();
 });
 
-// Route::middleware('auth:sanctum')->group(function () {
-Route::resource('ticket', TicketController::class);
-Route::put('/update_ticket_status/{ticket_id}', [TicketController::class, 'update_ticket_status']);
-Route::get('/get_categories_by_category/{category}', [TicketController::class, 'get_categories_by_category']);
-Route::get('/get_ticket_by_user_id/{userid}', [TicketController::class, 'get_ticket_by_user_id']);
-Route::get('/search_ticket/{search}', [TicketController::class, 'search_ticket']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('ticket', TicketController::class);
+    Route::put('/update_ticket_status/{ticket_id}', [TicketController::class, 'update_ticket_status']);
+    Route::get('/get_categories_by_category/{category}', [TicketController::class, 'get_categories_by_category']);
+    Route::get('/get_ticket_by_user_id/{userid}', [TicketController::class, 'get_ticket_by_user_id']);
+    Route::get('/search_ticket/{search}', [TicketController::class, 'search_ticket']);
 
 
-Route::post('/open_ticket_notification', [OpenTicketNotidicationController::class, 'open_ticket_notification']);
+    Route::post('/open_ticket_notification', [OpenTicketNotidicationController::class, 'open_ticket_notification']);
 
 
-Route::resource('user', UserController::class);
-Route::get('/get_user_by_position/{userid}', [UserController::class, 'get_user_by_position']);
-Route::resource('category', CategoryController::class);
-Route::resource('activity', ActivityController::class);
-Route::resource('note', NoteController::class);
-Route::resource('dashboard', DashboardController::class);
-
-// });
+    Route::resource('user', UserController::class);
+    Route::get('/get_user_by_position/{userid}', [UserController::class, 'get_user_by_position']);
+    Route::resource('category', CategoryController::class);
+    Route::resource('activity', ActivityController::class);
+    Route::resource('note', NoteController::class);
+    Route::resource('dashboard', DashboardController::class);
+});
